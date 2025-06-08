e9a22004da69ab9874886818350ca9ab6d9a8b32
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-06-08 17:46:57 +1000

    Update ndbtm.nix

--- programs/ndbtm.nix
+++ programs/ndbtm.nix
@@ -1,24 +1,31 @@
 { pkgs }:
-pkgs.symlinkJoin {
-  name = "ndbtm";
-  paths = [ pkgs.bottom ];
-  nativeBuildInputs = [ pkgs.makeWrapper ];
-  text = (pkgs.formats.toml { }).generate "btm.toml" {
-    flags = {
-      avg_cpu = true;
-      battery = true;
-      colors = { low_battery_color = "red"; };
-      enable_gpu_memory = true;
-      hide_table_gap = false;
-      hide_time = true;
-      rate = "1s";
-      temperature_type = "c";
-    };
+let
+  defaultFlags = {
+    avg_cpu = true;
+    battery = true;
+    colors = { low_battery_color = "red"; };
+    enable_gpu_memory = true;
+    hide_table_gap = false;
+    hide_time = true;
+    rate = "1s";
+    temperature_type = "c";
   };
-  postBuild = ''
-    mv $out/bin/btm $out/bin/ndbtm
-    wrapProgram $out/bin/ndbtm \
-      --add-flags "-C $text"
-    ln -s $out/bin/ndbtm $out/bin/btm
-  '';
-}
+  btmFinal = { flags }:
+    let
+      toml = (pkgs.formats.toml { }).generate "bottom.toml" { flags = flags; };
+      bin = pkgs.writeShellScriptBin "ndbtm" ''
+        exec ${pkgs.bottom}/bin/btm -C ${toml} "$@"
+      '';
+    in pkgs.stdenvNoCC.mkDerivation {
+      name = "ndbtm";
+      src = null;
+      dontUnpack = true;
+      dontConfigure = true;
+      dontBuild = true;
+      installPhase = ''
+        install -Dm644 ${toml}          $out/share/ndbtm/bottom.toml
+        install -Dm755 ${bin}/bin/ndbtm $out/bin/ndbtm
+        ln -s $out/bin/ndbtm $out/bin/btm
+      '';
+    };
+in pkgs.lib.makeOverridable btmFinal { flags = defaultFlags; }
