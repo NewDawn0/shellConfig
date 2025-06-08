376e9bd7b94fdf802694d375691415634bf2099b
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-06-08 17:47:27 +1000

    Update ndjq.nix

--- programs/ndjq.nix
+++ programs/ndjq.nix
@@ -1,26 +1,32 @@
 { pkgs }:
 let
-  jq = {
-    null = "1;30";
-    false = "0;37";
-    true = "0;37";
-    numbers = "0;37";
+  defaultTheme = {
+    null = "1;35";
+    false = "0;31";
+    true = "0;32";
+    numbers = "0;33";
     strings = "0;32";
-    arrays = "1;37";
-    objects = "1;37";
+    arrays = "1;36";
+    objects = "1;36";
   };
-in pkgs.symlinkJoin {
-  name = "ndjq";
-  paths = [
-    (pkgs.writeShellApplication {
+  jqWrapper = theme:
+    pkgs.writeShellApplication {
       name = "ndjq";
       text = ''
-        export JQ_COLORS="${jq.null}:${jq.false}:${jq.true}:${jq.numbers}:${jq.strings}:${jq.arrays}:${jq.objects}"
+        export JQ_COLORS="${theme.null}:${theme.false}:${theme.true}:${theme.numbers}:${theme.strings}:${theme.arrays}:${theme.objects}"
         exec ${pkgs.jq}/bin/jq "$@"
       '';
-    })
-  ];
-  postInstall = ''
-    ln -s $out/bin/ndjq $out/bin/jq
-  '';
-}
+    };
+  jqFinal = { theme }:
+    pkgs.stdenvNoCC.mkDerivation {
+      name = "jq-final";
+      pname = "jq";
+      dontUnpack = true;
+      dontConfigure = true;
+      dontBuild = true;
+      installPhase = ''
+        install -Dm755 ${jqWrapper theme}/bin/ndjq $out/bin/ndjq
+        ln      -s     $out/bin/ndjq               $out/bin/jq
+      '';
+    };
+in pkgs.lib.makeOverridable jqFinal { theme = defaultTheme; }
