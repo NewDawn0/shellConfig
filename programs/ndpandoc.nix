6b4ba31be2da242cb460797e7e81bdb00f849f67
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-06-08 17:47:54 +1000

    Update ndpandoc.nix

--- programs/ndpandoc.nix
+++ programs/ndpandoc.nix
@@ -1,23 +1,29 @@
 { pkgs }:
 let
   defaultConfig = {
-    author = "NewDawn0";
-    language = "en";
-    toc = true;
-  };
-in pkgs.lib.makeOverridable ({ config ? defaultConfig }:
-  pkgs.symlinkJoin {
-    name = "ndpandoc";
-    paths = with pkgs; [ pandoc ];
-    nativeBuildInputs = with pkgs; [ makeWrapper ];
-    text = (pkgs.formats.json { }).generate "pandoc.json" {
-      metadata = { inherit (config) author language toc; };
+    metadata = {
+      author = "NewDawn0";
+      language = "en";
+      toc = true;
     };
-    postBuild = ''
-      install -Dm644 $text $out/share/pandoc/pandoc.json
-      mv $out/bin/pandoc $out/bin/ndpandoc
-      wrapProgram $out/bin/ndpandoc \
-        --add-flags "--defaults $out/share/pandoc/pandoc.json"
-      ln -s $out/bin/ndpandoc $out/bin/pandoc
-    '';
-  }) { config = defaultConfig; }
+  };
+  pandocFinal = { config }:
+    let
+      json = (pkgs.formats.json { }).generate "pandoc.json" defaultConfig;
+      wrapper = pkgs.writeShellScriptBin "ndpandoc" ''
+        exec ${pkgs.pandoc}/bin/pandoc --defaults ${json} "$@"
+      '';
+    in pkgs.stdenvNoCC.mkDerivation {
+      name = "pandoc-final";
+      pname = "pandoc";
+      src = null;
+      dontUnpack = true;
+      dontConfigure = true;
+      dontBuild = true;
+      installPhase = ''
+        install -Dm644 ${json}                 $out/share/ndpandoc/pandoc.json
+        install -Dm755 ${wrapper}/bin/ndpandoc $out/bin/ndpandoc
+        ln      -s     $out/bin/ndpandoc       $out/bin/pandoc
+      '';
+    };
+in pkgs.lib.makeOverridable pandocFinal { config = defaultConfig; }
